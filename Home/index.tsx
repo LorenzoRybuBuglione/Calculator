b7b901d191d3366ac4958d7c5470e256b53deace
LorenzoRB <55191240+LorenzoRybuBuglione@users.noreply.github.com>
2023-03-27 11:58:50 -0300

    first commit :sparkles:

---
+++ Home/index.tsx
@@ -0,0 +1,16 @@
+
+
+import { Container, NumberButton, ButtonLabel } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+        <NumberButton>
+
+      <ButtonLabel>oi
+      </ButtonLabel>
+        </NumberButton>
+    </Container>
+  );
+}
+
---
+++ Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #2C3333;
+    align-items: center;
+    justify-content: center;
+ `;
+
+ export const NumberButton = styled.TouchableOpacity`
+    width: 100px;
+    height: 100px;
+    background-color: #2E4F4F;
+    align-items: center;
+    justify-content: center;
+ `;
+
+ export const ButtonLabel = styled.Text`
+    color: #CBE4DE;
+ `;
